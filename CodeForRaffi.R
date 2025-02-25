53c7ee037a71f2a06a79c75415c41c9a5936c40c
RSherak <rsherak@gmail.com>
2025-02-25 09:35:59 -0500

    adding code to incorperate 54 (53 when dropping one with missing location) arrests that were origninally unable to be geocoded automatically.
    Changed how code flows re: date since third data portion import dates differently.
    added intermediate step to show total pts prior to exclusions

--- CodeForRaffi.R
+++ CodeForRaffi.R
@@ -7,15 +7,16 @@ library(tidycensus)
 library(tigris)
 
 ## DATA 
-data1 <- read.csv("Matched_geocoded_xy.csv") %>% 
+data1 <- read.csv("data_geocoding/Matched_geocoded_xy.csv") %>% 
   dplyr::select(Incident__, Date_of_Arrest, Location_Type,
                 Arrest_Witness_Status,Age, Age_Modifier, Gender, 
                 Race_Ethnicity, Etiology=Presumed_Cardiac_Arrest_Etiology, 
                 Initiated_CPR, Type_of_Bystander_CPR_Provided, 
                 AED_applied=Was_an_AED_applied_prior_to_EMS_arrival,
-                x_text, y_text) 
+                x_text, y_text) %>% mutate(Date_of_Arrest = parse_date(Date_of_Arrest, "%m/%d/%y"))
 
-data2 <- read.csv("Tied_geocoded_xy.csv") %>% 
+
+data2 <- read.csv("data_geocoding/Tied_geocoded_xy.csv") %>% 
   mutate(Incident_Zip_Code = Fixed_Zip_text) %>% 
   dplyr::select(Incident__, Date_of_Arrest, Location_Type,
                 Arrest_Witness_Status, Age, Age_Modifier, Gender, 
@@ -24,9 +25,19 @@ data2 <- read.csv("Tied_geocoded_xy.csv") %>%
                 AED_applied=Was_an_AED_applied_prior_to_EMS,
                 x_text, y_text) %>%
   mutate(Date_of_Arrest = substr(Date_of_Arrest, 1, 
-                                 nchar(Date_of_Arrest)-5))
+                                 nchar(Date_of_Arrest)-5)) %>% 
+  mutate(Date_of_Arrest = parse_date(Date_of_Arrest, "%m/%d/%y"))
 
-dt.pre <- rbind(data1, data2) %>%
+data3 <- read_csv("data_geocoding/unmatchedgeocoded.csv", col_types = cols(Date_of_Arrest = col_date(format = "%m/%d/%Y"))) %>% 
+  filter(Incident_Address != "Unkown123") %>% #remove 1 arrest without location
+  dplyr::select(Incident__, Date_of_Arrest, Location_Type,
+                Arrest_Witness_Status,Age, Age_Modifier, Gender, 
+                Race_Ethnicity, Etiology=Presumed_Cardiac_Arrest_Etiology, 
+                Initiated_CPR, Type_of_Bystander_CPR_Provided, 
+                AED_applied=Was_an_AED_applied_prior_to_EMS_arrival,
+                x_text, y_text)
+#total data set
+dt.pre1 <- rbind(data1, data2, data3) %>%
   mutate(
     Initiated_bin = case_when(
       Initiated_CPR == "Bystander" | 
@@ -42,10 +53,15 @@ dt.pre <- rbind(data1, data2) %>%
       Race_Ethnicity == "Hispanic/Latino" ~ "Latino", 
       Race_Ethnicity == "White" ~ "White", 
       TRUE ~ "Other"),
-    Race_Binary = ifelse(Race_Ethnicity == "White", "White", "Non-White"), 
-    Date = parse_date(Date_of_Arrest, "%m/%d/%y")) %>%
-  add_count(Incident__) %>%
-  # EXCLUSIONS
+    Race_Binary = ifelse(Race_Ethnicity == "White", "White", "Non-White")) %>%
+  add_count(Incident__)
+
+# save all arrests file
+# save(dt.pre1, file = "allarrests.RData")
+
+ #restricted data set 
+dt.pre<- dt.pre1 %>% 
+    # EXCLUSIONS
   filter(Location_Type != "Healthcare Facility" & 
            Location_Type != "Nursing Home", 
          # Remove Not Applicable for now, while waiting for EMS to investigate
@@ -55,7 +71,7 @@ dt.pre <- rbind(data1, data2) %>%
          n == 1, # remove any duplicate incident IDs
          Gender != "Male-to-Female, Transgender Female",
          Gender != "", 
-         Date >= "2022-01-01", # 2022 onward
+         Date_of_Arrest >= "2022-01-01", # 2022 onward
          y_text < 41.5)
 
 # convert OHCA data to spatial points
@@ -74,7 +90,7 @@ points.sp@data$CENSUS <- sp::over(points.sp, as_Spatial(ctcensus))$NAME
 
 ## FINAL DATA 
 dt <- left_join(points.sp@data, dt.pre) %>% 
-  mutate(YEAR = year(Date), 
+  mutate(YEAR = year(Date_of_Arrest), 
          age_10 = Age/10, 
          Bystander_bin = ifelse(Initiated_bin == "Responder", 0, 1)) %>%
   filter(!is.na(CENSUS)) 
